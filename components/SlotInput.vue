bf9ec261c8033042574a69e3aca6cadcb54e9b6e
adrielwerlich <awebdev6@gmail.com>
2023-10-06 17:13:26 -0300

    reusable component input

---
+++ components/SlotInput.vue
@@ -0,0 +1,36 @@
+<!-- create a reusable input with label and customizable with props -->
+<template>
+  <div class="custom-input">
+    <label :for="id">{{ label }}</label>
+    <input
+      :id="id"
+      :type="type"
+      :name="name"
+      :value="value"
+      v-bind="$attrs"
+      v-on="$listeners"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref, defineProps } from 'vue';
+
+const props = defineProps(['id', 'label', 'type', 'value', 'name']);
+const id = ref(props.id || '');
+const label = ref(props.label || '');
+const type = ref(props.type || 'text');
+const value = ref(props.value || '');
+const name = ref(props.name || '');
+</script>
+
+<style scoped>
+.custom-input {
+  margin-bottom: 1rem;
+}
+
+label {
+  display: block;
+  margin-bottom: 0.5rem;
+}
+</style>
--- pages/sell/index.vue
+++ pages/sell/index.vue
@@ -1,3 +1,38 @@
 <template>
-    <h1>sell</h1>
+  <h1>sell</h1>
+
+  <div>
+    <SlotInput
+      id="first-name"
+      label="First name"
+      :type="'text'"
+      :value="inputValue"
+      name="firstName"
+      placeholder="Name"
+      @input="handleInput"
+    />
+  </div>
+  <div>
+    <SlotInput
+      id="last-name"
+      label="Last name"
+      :type="'text'"
+      :value="inputValue"
+      name="lastName"
+      placeholder="Last Name"
+      @input="handleInput"
+    />
+  </div>
 </template>
+
+<script setup></script>
+
+<script setup>
+import SlotInput from "~/components/SlotInput.vue";
+import { ref } from "vue";
+
+const inputValue = ref("");
+const handleInput = (event) => {
+  inputValue.value = event.target.value;
+};
+</script>
